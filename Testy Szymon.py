fff191ab153dc2f866ad22bd389611957995ff55
szymszm <szymon_zaczek@Interia.pl>
2024-06-06 18:20:42 +0200

    123

--- Testy Szymon.py
+++ Testy Szymon.py
@@ -1,18 +1,41 @@
-from Dane_IO import Fabryka_wejscia, Wejscie_xlsx
-from PanstwaM import Panstwo, Lista_panstw
-from STALE import plik_z_danymi
+import sys
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 
-czytajnik = Fabryka_wejscia()
-czytaj_xlsx = czytajnik.daj_wejscie(plik_z_danymi)
-lista = czytaj_xlsx.czytaj()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
 
-print(lista)
+        # Ustawienia głównego okna
+        self.setWindowTitle("Przykład Wykresu w PyQt6")
+        self.setGeometry(100, 100, 800, 600)
 
-lista = lista.daj_panstwa()
+        # Tworzenie QWidget jako centralnego widgetu
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
 
+        # Tworzenie layoutu
+        layout = QVBoxLayout()
+        central_widget.setLayout(layout)
 
+        # Tworzenie wykresu Matplotlib
+        self.figure, self.ax = plt.subplots()
+        self.canvas = FigureCanvas(self.figure)
+        layout.addWidget(self.canvas)
 
-for i in range(len(lista)):
-    print(lista[i].daj_nazwa())
-#masz <3
+        # Dodanie przykładowych danych do wykresu
+        self.plot()
 
+    def plot(self):
+        x = [0, 1, 2, 3, 4, 5]
+        y = [0, 1, 4, 9, 16, 25]
+        self.ax.plot(x, y)
+        self.ax.set_title("Przykładowy Wykres")
+        self.canvas.draw()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
